57146ff4e0d9cb3d61fc07bec8f1e0916fe12aca
Héctor Salinas <hector_sag@outlook.com>
2022-05-06 18:26:00 -0500

    Changed home layout

--- lib/kemet_web/views/page_view.ex
+++ lib/kemet_web/views/page_view.ex
@@ -1,3 +1,9 @@
 defmodule KemetWeb.PageView do
   use KemetWeb, :view
+
+  defp get_admin_name(conn) do
+    if conn.assigns[:current_admin] do
+      conn.assigns[:current_admin].email |> String.replace("@kemet.com.mx", "")
+    end
+  end
 end
