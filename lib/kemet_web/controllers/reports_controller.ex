883af6e29ef071463bf052a65c857bb8fc3d3efa
Héctor Salinas <hector_sag@outlook.com>
2022-05-16 11:51:48 -0500

    Added reports view

--- lib/kemet_web/controllers/reports_controller.ex
+++ lib/kemet_web/controllers/reports_controller.ex
@@ -1,7 +1,11 @@
 defmodule KemetWeb.ReportController do
   use KemetWeb, :controller
+  alias Kemet.Employees
+  alias Kemet.Productions
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    employees = Employees.list_active_employees()
+    productions = Productions.list_productions()
+    render(conn, "index.html", employees: employees, productions: productions)
   end
 end
--- lib/kemet_web/views/report_view.ex
+++ lib/kemet_web/views/report_view.ex
@@ -1,3 +1,34 @@
 defmodule KemetWeb.ReportView do
   use KemetWeb, :view
+
+  defp get_produced_boxes(conn) do
+    productions = conn.assigns[:productions]
+    case length(productions) do
+      0 -> 0
+      _ ->
+          Enum.map(productions , fn production -> production.capacitor_boxes end)
+          |> Enum.sum
+    end
+  end
+
+  defp get_used_machines(conn) do
+    productions = conn.assigns[:productions]
+    case length(productions) do
+      0 -> 0
+      _ ->
+          Enum.map(productions , fn production -> production.machine_name end)
+          |> Enum.uniq()
+          |> length()
+    end
+  end
+
+  defp get_chart_data(conn) do
+    productions = conn.assigns[:productions]
+    case length(productions) do
+      0 -> []
+      _ ->
+          Enum.map(productions , fn production -> [production.inserted_at, production.capacitor_boxes] end)
+          |> Jason.encode!()
+    end
+  end
 end
