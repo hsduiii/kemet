f7451c7e4ff54a9e2f3f08d4750c0b371de655c4
Héctor Salinas <hector_sag@outlook.com>
2022-05-19 17:01:20 -0500

    Fix assets not working on heroku

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,11 +48,11 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "localhost"
+  host = System.get_env("PHX_HOST") || "https://hsduiii-kemet.herokuapp.com/"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :kemet, KemetWeb.Endpoint,
-    url: [host: "https://hsduiii-kemet.herokuapp.com/", port: 443, scheme: "https", force_ssl: [rewrite_on: [:x_forwarded_proto]]],
+    url: [host: host, port: 443, scheme: "https", force_ssl: [rewrite_on: [:x_forwarded_proto]]],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
